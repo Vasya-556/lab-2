c4b3e44db84e9b7603bad6eedfb12005cc4475e0
Vasya-556 <vkobrin556@gmail.com>
2023-03-13 17:43:33 +0200

    first commit

---
+++ lab-2/lab-2.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+void table() {
+	cout << "=============" << endl;
+	cout << "|p|q|r|func |" << endl;
+	for (int p = 0; p < 2; p++)
+		for (int q = 0; q < 2; q++)
+			for (int r = 0; r < 2; r++) {
+				cout << "-------------" << endl;
+				cout << "|" << p << "|" << q << "|" << r << "|" << ((!(p && q) || (p && r)) && (!(p && r) || (p && q))) << "    |" << endl;
+			}
+	cout << "=============" << endl;
+}
+
+int main()
+{
+	table();
+}
